cb32d522b621d4418fd2f3d3bbd34a01bdf5f2d5
vidalvanbergen <vidal.vanbergen@gmail.com>
2010-06-17 14:00:30 +0200

    Some adjustments, support for Helvetireader and a short commentary.

--- Better Google Reader.safariextension/articlefocus.js
+++ Better Google Reader.safariextension/articlefocus.js
@@ -1,5 +1,5 @@
 (function() {
-var css = "#viewer-footer, #viewer-details-toggle, #viewer-header, #chrome-header, .loaded #search, #guser { display: none; !important }";
+var css = "#viewer-footer, #viewer-details-toggle, #viewer-header, #chrome-header, .loaded #search, #guser, #gbar, .gbh, #logo-container { display: none; !important } #main { top: 0px !important; } #chrome { margin-top: 0px !important; }";
 var heads = document.getElementsByTagName("head");
 if (heads.length > 0) {
 	var node = document.createElement("style");
@@ -8,3 +8,7 @@ if (heads.length > 0) {
 	heads[0].appendChild(node); 
 }
 })();
+
+// Hide unwanted elements with {Dislay: none}.
+// Set the main view's top to 0 to stretch across the screen.
+// Set Chrome's margin-top override for Helvetireader support, and possibly others.
